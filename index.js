33f53ea2da795d1504727d3b6bc99750c2cff0e5
ryjtoh <ryjtoh@gmail.com>
2022-04-13 10:48:32 -0700

    finishing touches made (rereading everything one more time)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("canvas") 
 const ctx = canvas.getContext("2d")
 
+// Premade holes for the user to solve using sliders
 var holes = 
     {
         "holeSpots":[
@@ -11,7 +12,6 @@ var holes =
         ]
     }
 
-console.log(holes.holeSpots[2].x1)
 
 canvas.width = 600
 canvas.height = 600
@@ -46,6 +46,7 @@ var emptyGrid
 // Function to create grid
 function drawBoard() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // Draw the coordinate system lines
     for (var x = 0; x <= sideLength; x += unit) {
         ctx.moveTo(0.5 + x, 0)
         ctx.lineTo(0.5 + x, sideLength)
@@ -56,26 +57,70 @@ function drawBoard() {
     ctx.lineCap = "round"
     ctx.stroke()
 
-    blueHoleX = holes.holeSpots[holeTurn].x1
-    blueHoleY = holes.holeSpots[holeTurn].y1
-    redHoleX = holes.holeSpots[holeTurn].x2
-    redHoleY = holes.holeSpots[holeTurn].y2
-    holeSlope = holes.holeSpots[holeTurn].slope
-    holeYInt = holes.holeSpots[holeTurn].yInt
-    holeTurn++
-    if (holeTurn > 3) {
-        holeTurn = 0
-    }
-    drawHole(blueHoleX, blueHoleY, "#29648a")
-    drawHole(redHoleX, redHoleY, "#6e1014")
+    // Draw the preset holes
+    createHoles()
 
+    // Save state
     emptyGrid = ctx.getImageData(0, 0, canvas.width, canvas.height)
     document.getElementById("slope").value = 1
     document.getElementById("showYInt").value = 0
+    // Draw y = x
     drawDefaultSlope()
 }
 
+// Draws the grid with preset holes and default slope line
 drawBoard()
+
+// Save this "base" state to help with animation
+var currEquation = ctx.getImageData(0, 0, canvas.width, canvas.height)
+// Sliders values for slope and y-intercept
+var slopeSlider = document.getElementById("slope")
+var yInterceptSlider = document.getElementById("y-intercept")
+
+// Changes the HTML text of the equation in real time to show changes
+function showEquation() {
+    document.getElementById("showSlope").innerHTML = slopeSlider.value
+    document.getElementById("showYInt").innerHTML = yInterceptSlider.value
+}
+
+// Function to change line when either slider is updated
+function changeLine() {
+    calibrateSwing()
+    showEquation()
+    // Restore to empty grid
+    ctx.putImageData(emptyGrid, 0, 0)
+    // Gather slider values
+    var slope = slopeSlider.value
+    var yIntercept = yInterceptSlider.value * unit
+    // Find endpoints of line
+    var x1 = 1500 - yIntercept
+    var y1 = findY1(slope) - yIntercept
+    var x2 = -900 - yIntercept
+    var y2 = findY2(slope) - yIntercept
+    // Draw the line and center dot
+    drawLine(centerWidth - yIntercept, centerHeight - yIntercept, x1, y1, "#29648a")
+    drawLine(centerWidth - yIntercept, centerHeight - yIntercept, x2, y2, "#6e1014")
+    drawCircle(centerWidth - yIntercept, centerHeight - yIntercept, 5, "#aaabb8")
+    // Save this state of grid
+    currEquation = ctx.getImageData(0, 0, canvas.width, canvas.height)
+}
+
+
+
+//////////////////////
+// HELPER FUNCTIONS //
+//////////////////////
+
+
+
+// Finding the y value for the ending point of the line
+function findY1(slope) {
+    return (slope * 1200) * -1 + 300
+}
+// Finding the y value for the starting point of the line
+function findY2(slope) {
+    return (slope * 1200) + 300
+}
 
 // Draw default slope lines (y = x)
 function drawDefaultSlope() {
@@ -95,102 +140,42 @@ function drawDefaultSlope() {
     drawCircle(centerWidth, centerHeight, 5, "#aaabb8")
 }
 
-
-
-
-var currEquation = ctx.getImageData(0, 0, canvas.width, canvas.height)
-// Sliders values for slope and y-intercept
-var slopeSlider = document.getElementById("slope")
-var yInterceptSlider = document.getElementById("y-intercept")
-
-function showEquation() {
-    document.getElementById("showSlope").innerHTML = slopeSlider.value
-    document.getElementById("showYInt").innerHTML = yInterceptSlider.value
+// Draws a circle based on coordinates, size, and color
+function drawCircle(x, y, size, color) {
+    ctx.beginPath()
+    ctx.arc(x, y, size, 0, Math.PI*2)
+    ctx.fillStyle = color
+    ctx.fill()
+    ctx.closePath()
 }
 
-
-// Function to change line when either slider is updated
-function changeLine() {
-    calibrateSwing()
-    showEquation()
-    // Restore to empty grid
-    ctx.putImageData(emptyGrid, 0, 0)
-    // Gather slider values
-    var slope = slopeSlider.value
-    var yIntercept = yInterceptSlider.value * unit
-    // Find endpoints of line
-    var y1 = findY1(slope)
-    var y2 = findY2(slope)
-
-    // Draw blue
+// Draws a line based on start and end points and color
+function drawLine(x1, y1, x2, y2, color) {
     ctx.beginPath()
-    ctx.moveTo(centerWidth - yIntercept, centerHeight - yIntercept)
-    ctx.lineTo(1500 - yIntercept, y1 - yIntercept)
-    ctx.strokeStyle = "#29648a"
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
     ctx.lineWidth = 5
     ctx.stroke()
-    // Draw red
-    ctx.beginPath()
-    ctx.moveTo(centerWidth - yIntercept, centerHeight - yIntercept)
-    ctx.lineTo(-900 - yIntercept, y2 - yIntercept)
-    ctx.strokeStyle = "#6e1014"
-    ctx.stroke()
     ctx.lineWidth = 1
-
-    drawCircle(centerWidth - yIntercept, centerHeight - yIntercept, 5, "#aaabb8")
-    currEquation = ctx.getImageData(0, 0, canvas.width, canvas.height)
-    
 }
 
-// Finding the y value for the ending point of the line
-function findY1(slope) {
-    return (slope * 1200) * -1 + 300
-}
-// Finding the y value for the starting point of the line
-function findY2(slope) {
-    return (slope * 1200) + 300
-}
-
-function drawCircle(x, y, size, color) {
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI*2);
-    ctx.fillStyle = color;
-    ctx.fill();
-    ctx.closePath();
-}
-
-// Blue golf ball graphics
-function drawBlueBall() {
-    ctx.beginPath();
-    ctx.arc(blueBallX, blueBallY, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#29648a";
-    ctx.fill();
-    ctx.closePath();  
-}
-// Red golf ball graphics
-function drawRedBall() {
-    ctx.beginPath();
-    ctx.arc(redBallX, redBallY, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#6e1014";
-    ctx.fill();
-    ctx.closePath();
-}
 // Draw the next golf ball frame
 function draw() {
     ctx.putImageData(emptyGrid, 0, 0)
-    drawBlueBall()
-    drawRedBall()
-    blueBallX += dx;
-    blueBallY += dy;
-    redBallX -= dx;
-    redBallY -= dy;
+    drawCircle(blueBallX, blueBallY, 10, "#29648a")
+    drawCircle(redBallX, redBallY, 10, "#6e1014")
+    blueBallX += dx
+    blueBallY += dy
+    redBallX -= dx
+    redBallY -= dy
 }
 
 // Responds to button to put balls in motion
 function startSwing() {
     calibrateSwing()
     let swung = setInterval(() => draw(), 10 + dy * dy)
-    setTimeout(() => { clearInterval(swung), outcome() }, 4000);
+    setTimeout(() => { clearInterval(swung), outcome() }, 4000)
 }
 // Changes the starting ball position and track angle based on slider fields
 function calibrateSwing() {
@@ -215,17 +200,11 @@ function createHoles() {
     if (holeTurn > 3) {
         holeTurn = 0
     }
-    drawHole(blueHoleX, blueHoleY, "#29648a")
-    drawHole(redHoleX, redHoleY, "#6e1014")
+    drawCircle(blueHoleX, blueHoleY, 10, "#29648a")
+    drawCircle(redHoleX, redHoleY, 10, "#6e1014")
 }
 
-function drawHole(x, y, color) {
-    ctx.beginPath()
-    ctx.arc(x, y, 10, 0, Math.PI*2)
-    ctx.fillStyle = color
-    ctx.fill()
-}
-
+// Displays the outcome of the user's line, and resets the board after alert is done
 function outcome() {
     var message = ""
     if (slopeSlider.value == holeSlope && yInterceptSlider.value == holeYInt) {
